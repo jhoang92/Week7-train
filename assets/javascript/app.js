99f12f1cb530827006f1bd1d39ff32822f90d678
jhoang92 <pergexplr@gmail.com>
2018-04-02 15:18:26 -0600

    added initial setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDH_OYKSSzBVtLQLjofJ58B5nhL3r6OZnI",
+    authDomain: "trainsheet-2b137.firebaseapp.com",
+    databaseURL: "https://trainsheet-2b137.firebaseio.com",
+    projectId: "trainsheet-2b137",
+    storageBucket: "trainsheet-2b137.appspot.com",
+    messagingSenderId: "378495248609"
+  };
+  firebase.initializeApp(config);
